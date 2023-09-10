fa2d629638eb71c9c8aad4c116262d963753c3a7
lavi1205 <thinhphamthe874@gmail.com>
2023-09-11 01:49:50 +0700

    update file for s3

--- airflow_dag/dags/weather_dag.py
+++ airflow_dag/dags/weather_dag.py
@@ -1,5 +1,6 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime
 import sys
 sys.path.append("task")
--- airflow_dag/task/weather_extraction.py
+++ airflow_dag/task/weather_extraction.py
@@ -38,57 +38,64 @@ def check_api_request(api_url):
 
 
 def extract_data(api_url):
-    response = requests.get(api_url).json()
-    for key in response:
-        city = response.get('name')
-        temperature       = round(convert_temperature(int(response.get('main').get('temp'))),2)
-        temperature_min   = round(convert_temperature(int(response.get('main').get('temp_min'))),2)
-        temperature_max   = round(convert_temperature(int(response.get('main').get('temp_max'))),2)
-        pressure          = response.get('main').get('pressure')
-        humidity          = response.get('main').get('humidity')
-        description       = response.get('weather')[0].get('description')
-        sunrise           = convert_time(response.get('sys').get('sunrise'))
-        sunset            = convert_time(response.get('sys').get('sunset'))
-        local_record_time = convert_time(response.get('dt'))
-        transformed_data  = {
-            "City": city,
-            "Description": description,
-            "Temperature":temperature,
-            "Maximum Temperature (C)": temperature_max,
-            "Minimum Temperature (C)": temperature_min,
-            "Pressure": pressure,
-            "Humidity": humidity,
-            "Sunrise (Local Time)":sunrise,
-            "Sunset (Local Time)": sunset,
-            "Local Record Time": local_record_time
-        }
-        transformed_data_list = [transformed_data]
-        df = pd.DataFrame.from_dict(transformed_data_list)
-        now = datetime.datetime.now() 
-        # Define the desired time zone
-        desired_timezone = pytz.timezone('Etc/GMT+7')  # Replace with your desired time zone
-        # Convert the datetime object to the desired time zone
-        now_in_desired_timezone = now.astimezone(desired_timezone)
-        time_file_create = now_in_desired_timezone.strftime("%d%m%Y%H%M%S")
-        file_name = 'Current_weather_data_' + time_file_create + '.csv'
-        print(f'file containe weather data has been create with name {file_name}')
-        df.to_csv(file_name)
-        return file_name
+    try:
+        response = requests.get(api_url).json()
+        for key in response:
+            city = response.get('name')
+            temperature       = round(convert_temperature(int(response.get('main').get('temp'))),2)
+            temperature_min   = round(convert_temperature(int(response.get('main').get('temp_min'))),2)
+            temperature_max   = round(convert_temperature(int(response.get('main').get('temp_max'))),2)
+            pressure          = response.get('main').get('pressure')
+            humidity          = response.get('main').get('humidity')
+            description       = response.get('weather')[0].get('description')
+            sunrise           = convert_time(response.get('sys').get('sunrise'))
+            sunset            = convert_time(response.get('sys').get('sunset'))
+            local_record_time = convert_time(response.get('dt'))
+            transformed_data  = {
+                "City": city,
+                "Description": description,
+                "Temperature":temperature,
+                "Maximum Temperature (C)": temperature_max,
+                "Minimum Temperature (C)": temperature_min,
+                "Pressure": pressure,
+                "Humidity": humidity,
+                "Sunrise (Local Time)":sunrise,
+                "Sunset (Local Time)": sunset,
+                "Local Record Time": local_record_time
+            }
+            transformed_data_list = [transformed_data]
+            df = pd.DataFrame.from_dict(transformed_data_list)
+            now = datetime.datetime.now() 
+            # Define the desired time zone
+            desired_timezone = pytz.timezone('Etc/GMT+7')  # Replace with your desired time zone
+            # Convert the datetime object to the desired time zone
+            now_in_desired_timezone = now.astimezone(desired_timezone)
+            time_file_create = now_in_desired_timezone.strftime("%d%m%Y%H%M%S")
+            file_name = 'Current_weather_data_' + time_file_create + '.csv'
+            df.to_csv(file_name)
+            print(f'file containe weather data has been create with name {file_name}')
+            return file_name
+    except Exception as e:
+        print(f"tranform data has fail with error: {str(e)}")
 
 def move_data_s3(api_url):
-    # Initialize the S3 client
-    client = boto3.client('s3', aws_access_key_id='AKIA2EESKAGRR3BNPVNE',
-    aws_secret_access_key='Dq1gUJJBXphBkIxURQTPsxPi9N1AmvFLH/jJuyOZ')
-    
-    # Specify the S3 bucket name
-    bucket_name = 'thinh123'  # Replace with your actual bucket name
-    
-    # Call the extract_data function to get the file name
-    file_name = extract_data(api_url)
-    
-    # Upload the file to S3
-    response = client.upload_file(
-        file_name,  # Use the file name returned by extract_data
-        bucket_name,
-        file_name  # Specify the S3 object key (file name)
-    )
+    try:
+        # Initialize the S3 client
+        client = boto3.client('s3', aws_access_key_id='AKIA2EESKAGRR3BNPVNE',
+        aws_secret_access_key='Dq1gUJJBXphBkIxURQTPsxPi9N1AmvFLH/jJuyOZ')
+        
+        # Specify the S3 bucket name
+        bucket_name = 'thinh123'  # Replace with your actual bucket name
+        
+        # Call the extract_data function to get the file name
+        file_name = extract_data(api_url)
+        
+        # Upload the file to S3
+        response = client.upload_file(
+            file_name,  # Use the file name returned by extract_data
+            bucket_name,
+            file_name  # Specify the S3 object key (file name)
+        )
+    except Exception as e:
+        print(f"Move csv file to s3 encountered an error: {str(e)}")
+
